47c0aeee5fc1e47c72ceccf3689667cff8df3da0
Vojtech Mares <iam@vojtechmares.com>
2024-05-10 13:58:09 +0200

    refactor: finalizer format

--- internal/controller/finalizer.go
+++ internal/controller/finalizer.go
@@ -7,12 +7,12 @@ import (
 	networkingv1 "k8s.io/api/networking/v1"
 )
 
-const ingressFinalizer = "finalizer.cloudflare-tunnel-ingress-controller.clbs.io"
+const ingressTunnelFinalizer = "finalizer.cloudflare-tunnel-ingress-controller.clbs.io/tunnel"
 
 func (c *IngressController) ensureFinalizers(ctx context.Context, logger logr.Logger, ing *networkingv1.Ingress) error {
 	containsFinalizer := false
 	for _, finalizer := range ing.GetFinalizers() {
-		if finalizer == ingressFinalizer {
+		if finalizer == ingressTunnelFinalizer {
 			containsFinalizer = true
 			break
 		}
@@ -20,11 +20,11 @@ func (c *IngressController) ensureFinalizers(ctx context.Context, logger logr.Lo
 
 	if !containsFinalizer {
 		logger.Info("Adding Finalizer for the Ingress resource")
-		ing.SetFinalizers(append(ing.GetFinalizers(), ingressFinalizer))
+		ing.SetFinalizers(append(ing.GetFinalizers(), ingressTunnelFinalizer))
 
 		err := c.client.Update(ctx, ing)
 		if err != nil {
-			logger.Error(err, "Failed to update Ingress resource with a finalizer", "finalizer", ingressFinalizer)
+			logger.Error(err, "Failed to update Ingress resource with a finalizer", "finalizer", ingressTunnelFinalizer)
 			return err
 		}
 	}
